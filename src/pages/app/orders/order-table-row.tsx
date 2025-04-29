cf636da9f4340d8ef490a92629455402cafb81ee
Renato-Rodrigues83 <renatinprado8832@gmail.com>
2025-04-29 11:54:51 -0300

    Add charts & 404 page

--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -1,15 +1,22 @@
 import { Button } from "@/components/ui/button";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
 import { TableRow, TableCell } from "@/components/ui/table";
 import { Search, ArrowRight, X } from "lucide-react";
+import { OrderDetails } from "./order-details";
 
 export function OrderTableRow() {
     return (
         <TableRow>
             <TableCell>
-                <Button variant="outline" size="xs">
-                    <Search className=" h-3 w-3" />
-                    <span className="sr-only">Detalhes do pedido</span>
-                </Button>
+                <Dialog>
+                    <DialogTrigger asChild>
+                        <Button variant="outline" size="xs">
+                            <Search className=" h-3 w-3" />
+                            <span className="sr-only">Detalhes do pedido</span>
+                        </Button>
+                    </DialogTrigger>
+                    <OrderDetails />
+                </Dialog>
             </TableCell>
             <TableCell className=" font-mono text-xs font-medium">
                 2334445555
@@ -17,9 +24,11 @@ export function OrderTableRow() {
             <TableCell className=" text-muted-foreground">
                 há 15 minutos
             </TableCell>
-            <TableCell className=" flex items-center gap-2">
-                <span className=" h-2 w-2 rounded-full bg-slate-400" />
-                <span className=" font-medium text-muted-foreground">Pendente</span>
+            <TableCell>
+                <div className=" flex items-center gap-2">
+                    <span className=" h-2 w-2 rounded-full bg-slate-400" />
+                    <span className=" font-medium text-muted-foreground">Pendente</span>
+                </div>
             </TableCell>
             <TableCell className=" font-medium">
                 Nome do Cliente
@@ -42,3 +51,4 @@ export function OrderTableRow() {
         </TableRow>
     )
 }
+
